50f3013e31e46f7429114c14de28fd07266857e2
guisbonddev <wguisbond@gmail.com>
2018-09-02 11:22:42 -0400

    updated image lengths

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,7 +7,7 @@
                       <li><a href="/contact">Contact</a></li>
                   </ul>
               </div></div>
-               <a class="uk-navbar-item uk-logo" href="/"><img src="/storage/logo.png" width="7%"></a>
+               <a class="uk-navbar-item uk-logo" href="/"><img src="https://lh3.googleusercontent.com/_OaZJ8CsJGc0-Dw1m2OQISU50-d92a0--JmwUBUuMcbUWAdBGWyxuC4kEP1JGsqpXqOMmXVkEN5rLG4Ix8M_S2EWJG81uzcMkT7lIAmMR6NLM00_6oLfV0asVSgxXl7rb8agUp-YU74JctiERi18Fac_jcJLiwyhM1hSQk1fmV2r1V8S10yKaE143Oc1V9jYrqWCVtab87uM-wQUzlYEbup0p4ZXDOA8nqSBXFi1cCkFRAsSZHsYT8wswfOAipxDuZ6lvBgHoq36rZBrE1db03KC2tESk45VX1hYWJ3KYtAscpot4xZfEheNyfMUaSPN7shl-5LtBHuQo38SncDPvKW255ifYpqCPCMw3tPi2Yh0k7pQaIgaLuC-8IyAtPLQimIrOWkcXFNMuL6vl3ii2MBuWtDX3AUPQyGII-Vao-Qli4w9Cqh6DHWEL_kH-TsE1CbpdF_DyUtuIycOZAqHmXXvWsJ2j8CCD2PJMDm5ODwbln-tWbC66zDqbkZlrb1PvWNYwgAQzXeRXsuuMD3WFv9dpFeZhmPl_7TUyZGo6FnokRGq2L7wW2mRpadn0ySyMBNi6UGoLCAsOnGlUzJfGdgvN61hZaB_=w1440-h826?pli=1&authuser=2" width="7%"></a>
               <div class="uk-navbar-center-right"><div>
                   <ul class="uk-navbar-nav">
                       <li><a href="/404">Contests</a></li>
