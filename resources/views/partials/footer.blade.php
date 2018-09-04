1afdf732205bde1812a4aaf6b2fd222145b9ed9b
guisbonddev <wguisbond@gmail.com>
2018-09-04 15:45:59 -0400

    added staff access link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
           <div class="uk-navbar-center">
             <div class="uk-navbar-center"><div>
               <ul class="uk-navbar-nav">
-                 <li><a href="#">Site by Will Guisbond</a></li>
+                 <li><a href="http://admin.jdlitmag.org/">Site by Will Guisbond</a></li>
                  <li><a class="inactive" href="#">&copy; Rambunctious Literary Magazine 2018-2019</a></li>
               </ul>
             </div></div>
