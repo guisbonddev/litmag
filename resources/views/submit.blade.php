b13b4fa97dbdc11866502c85c34132a23d2b6fd0
guisbonddev <wguisbond@gmail.com>
2018-09-04 15:35:10 -0400

    added include-based footer

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -137,17 +137,7 @@
             </center>
         </div>
       </div>
-      <nav class="uk-navbar-container uk-margin uk-navbar-transparent" uk-navbar>
-          <div class="uk-navbar-center">
-            <div class="uk-navbar-center"><div>
-              <ul class="uk-navbar-nav">
-                 <li><a href="#">Site by Will Guisbond</a></li>
-              </ul>
-            </div></div>
-          </div>
-        </div>
-      </nav>
-               
+      @include('partials.footer')               
 
 
     <!-- ///////////////////////////////////////////////////////////// -->
