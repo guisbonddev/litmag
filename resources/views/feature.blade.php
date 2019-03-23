5fe95c9caf50c41b42f8d91c6efa94b42f078b3d
guisbonddev <wguisbond@gmail.com>
2019-03-23 16:52:13 -0400

    Feature page for news

---
+++ resources/views/feature.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Rambunctious Literary Magazine</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/vendor.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script src="js/pace.min.js"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+</head>
+
+<body id="top">
+
+    <!-- pageheader
+    ================================================== -->
+    
+    @include('partials.nav')
+
+<section class="s-content s-content--narrow s-content--no-padding-bottom">
+    <article class="row format-gallery">
+
+        <div class="s-content__header col-full">
+            <h1 class="s-content__header-title">
+                JD Authors Take on Scholastic
+            </h1>
+            <ul class="s-content__header-meta">
+                <li class="date">March 23, 2019</li>
+                <br>
+                <li class="cat">
+                    Authors Sofia and Alethea show off their skills in pieces showcased in recent <i>Rambunctious</i> editions. Use the buttons below to read each of their pieces. We are so proud of them!
+                </li>
+            </ul>
+            <a class="btn btn--primary full-width" href="https://drive.google.com/file/d/0B_hxuOMlk60zb0pDN0kwZkhPRDQ/view?usp=sharing">Sofia Liaw, <i>Split Ends</i> </a>
+            <a class="btn btn--primary full-width" href="https://docs.google.com/document/d/e/2PACX-1vTt-LP2yy9YJejP9rQT7VHOzBLtMR9S_DxvUKRp2KMzU6xI1JbDEA1MY3dqwCaeKVvTCrqm-faLxSzn/pub">Alethea Shirilian-Howlett, <i>Peeling Oranges</i> </a>
+        </div> <!-- end s-content__header -->
+    </article><br>
+</section> <!-- end s-pageheader -->
+
+    @include('partials.footer')
+
+</body>
+
+</html>
